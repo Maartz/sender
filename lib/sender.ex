89a8964b77ed587c008220d358f58442401cedf6
William <w.martin38@icloud.com>
2021-04-15 22:20:34 +0200

    Base state to begin tinkering

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,18 +1,12 @@
 defmodule Sender do
-  @moduledoc """
-  Documentation for `Sender`.
-  """
+  def send_email(email) do
+    random_waiting_time = :crypto.rand_uniform(1000, 4000)
+    Process.sleep(random_waiting_time)
+    IO.puts("Email to #{email} sent")
+    {:ok, email}
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Sender.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def notify_all(emails) do
+    Enum.each(emails, &send_email/1)
   end
 end
