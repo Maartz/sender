451ff6891d3273b18a7b360c77c17267bbfa19a9
William <w.martin38@icloud.com>
2021-04-17 14:31:12 +0200

    Add crashing clause for tinkering

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,12 +1,18 @@
 defmodule Sender do
-  def send_email(email) do
+
+  def send_email("konnichiwa@world.com" = email), do:
+    raise "Oops, couldn't send email to #{email}!"
+
+ def send_email(email) do
     random_waiting_time = :crypto.rand_uniform(1000, 4000)
     Process.sleep(random_waiting_time)
     IO.puts("Email to #{email} sent")
     {:ok, email}
   end
-
   def notify_all(emails) do
-    Enum.each(emails, &send_email/1)
+    emails
+    |> Task.async_stream(&send_email/1, ordered: false, on_timeout: :kill_task)
+    |> Enum.to_list()
   end
+
 end
