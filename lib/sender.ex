2d5b9f93a8e51fc0d47d9593d7edd7c64eab40e7
William <w.martin38@icloud.com>
2021-04-17 15:36:22 +0200

    Leverage Task.Supervisor.async_stream_nolink to handle crash in process

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,17 +1,17 @@
 defmodule Sender do
+  def send_email("konnichiwa@world.com" = email),
+    do: raise("Oops, couldn't send email to #{email}!")
 
-  def send_email("konnichiwa@world.com" = email), do:
-    raise "Oops, couldn't send email to #{email}!"
-
- def send_email(email) do
+  def send_email(email) do
     random_waiting_time = :crypto.rand_uniform(1000, 4000)
     Process.sleep(random_waiting_time)
     IO.puts("Email to #{email} sent")
     {:ok, email}
   end
+
   def notify_all(emails) do
-    emails
-    |> Task.async_stream(&send_email/1, ordered: false, on_timeout: :kill_task)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
     |> Enum.to_list()
   end
 end
